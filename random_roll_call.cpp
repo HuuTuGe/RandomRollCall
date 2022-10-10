f807bf80980e42796b3696ae89840ad458b54b76
HuuTuGe <1017636154@qq.com>
2022-10-10 12:31:39 +0800

    增加抽点方案输出

--- random_roll_call.cpp
+++ random_roll_call.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 struct student {
+	int number=0;//号码 
 	double grade = 0;//绩点
 	double pastp = 0;//缺勤系数
 	double nownumber = 0;//现在缺勤次数
@@ -18,29 +19,29 @@ bool cmp(student x, student y)
 }
 
 double E = 0, numerator = 0, denominator = 0;
+string s1 = "软件工程", s2 = "数据库系统原理", s3 = "计算机操作系统", s4 = "计算机图形学", s5 = "大学应用写作";
 void read_save1();
 void read_save2();
 void read_save3();
 void read_save4();
 void read_save5();
 void final_result();
-void address_E();
-void resorting();
+void address_E(string s);
 void pastp_add(int x, int y);
 
 
 int main()
 {
 	read_save1();
-	address_E();
+	address_E(s1);
 	read_save2();
-	address_E();
+	address_E(s2);
 	read_save3();
-	address_E();
+	address_E(s3);
 	read_save4();
-	address_E();
+	address_E(s4);
 	read_save5();
-	address_E();
+	address_E(s5);
 	final_result();
 }
 
@@ -60,14 +61,17 @@ void pastp_add(int x, int y)
 		numerator++;
 	}
 }
-void address_E()
+void address_E(string s)
 {
+	cout << s << "的抽点方案如下:" << endl;
 	sort(stu, stu + 90, cmp);
 	//处理分子
 	for (int i = 0; i < 20; i++)//对于每一门课的20次课
 	{
+		cout <<"第"<< i+1 << "次课抽点的同学号码为：" ;
 		for (int j = 0; j < 5; j++)//7个
 		{
+			cout << stu[j].number << " ";
 			if (stu[j].here[i] == 0)//没到
 			{
 				numerator++;
@@ -78,12 +82,12 @@ void address_E()
 		if (temp == 2)//点了2个人
 		{
 			int num = (rand() % (7 - 5) + 6);//在除了前几个人里的排名前二抽一个
+			cout << stu[num].number << " ";
 			pastp_add(num, i);
 			denominator += 1;
 		}
 		else if (temp == 3)//点了3个人
 		{
-		
 			int num1 = (rand() % (9 - 5) + 6);//在除了前几个人里的排名前4抽两个
 			int num2 = (rand() % (9 - 5) + 6);
 			while (num2 == num1)
@@ -91,13 +95,17 @@ void address_E()
 				num2 = (rand() % (9 - 5) + 6);
 				if (num2 != num1) break;
 			}
+			cout << stu[num1].number << " ";
+			cout << stu[num2].number << " ";
 			pastp_add(num1, i);
 			pastp_add(num2, i);
 			denominator += 2;
 		}
+		cout << endl;
 	}
 	//处理分母
 	denominator += 100;//前5-8个取了5个
+	cout << endl;
 }
 
 void read_save1() //输入第一门课的所有数据
@@ -106,6 +114,7 @@ void read_save1() //输入第一门课的所有数据
 	infile.open("output1.txt");      //注意文件的路径  
 	for (int i = 0; i < 90; i++)
 	{
+		infile >> stu[i].number;
 		infile >> stu[i].grade;
 		infile >> stu[i].pastp;
 		for (int j = 0; j < 20; j++)
@@ -122,6 +131,7 @@ void read_save2() //输入第二门课的所有数据
 	infile.open("output2.txt");      //注意文件的路径  
 	for (int i = 0; i < 90; i++)
 	{
+		infile >> stu[i].number;
 		infile >> stu[i].grade;
 		infile >> stu[i].pastp;
 		for (int j = 0; j < 20; j++)
@@ -138,6 +148,7 @@ void read_save3()//输入第三门课的所有数据
 	infile.open("output3.txt");      //注意文件的路径  
 	for (int i = 0; i < 90; i++)
 	{
+		infile >> stu[i].number;
 		infile >> stu[i].grade;
 		infile >> stu[i].pastp;
 		for (int j = 0; j < 20; j++)
@@ -153,6 +164,7 @@ void read_save4()//输入第四门课的所有数据
 	infile.open("output4.txt");      //注意文件的路径  
 	for (int i = 0; i < 90; i++)
 	{
+		infile >> stu[i].number;
 		infile >> stu[i].grade;
 		infile >> stu[i].pastp;
 		for (int j = 0; j < 20; j++)
@@ -169,6 +181,7 @@ void read_save5()//输入第五门课的所有数据
 	infile.open("output5.txt");      //注意文件的路径  
 	for (int i = 0; i < 90; i++)
 	{
+		infile >> stu[i].number;
 		infile >> stu[i].grade;
 		infile >> stu[i].pastp;
 		for (int j = 0; j < 20; j++)
@@ -182,5 +195,5 @@ void read_save5()//输入第五门课的所有数据
 void final_result()
 {
 	E = numerator / denominator;
-	cout << E << endl;
+	cout << "E的值是：" << E << endl;
 }
